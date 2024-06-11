e6df8410903d7a3e7380c5abb6c3bf9835df93b4
gangxmods <160806005+gangxmods@users.noreply.github.com>
2024-06-10 20:22:20 -0400

    - sunset button - button CD

--- Buttons.cs
+++ Buttons.cs
@@ -1,33 +1,66 @@
-﻿using System.Collections;
+using System.Collections;
 using UnityEngine;
 
 namespace DaytimeAndNightTimeChanger
 {
     public class SunButton : GorillaPressableButton
     {
+        public static bool CD = false;
         private void OnTriggerEnter(Collider collider)
         {
-            StartCoroutine(Press());
+            if (!CD)
+            {
+                StartCoroutine(Press());
+            }
         }
 
         private IEnumerator Press()
         {
+            CD = true;
             Plugin.instance.ChangeToDay();
             yield return new WaitForSeconds(0.25f);
+            CD = false;
         }
     }
 
     public class MoonButton : GorillaPressableButton
     {
+
+        public static bool CD = false;
         private void OnTriggerEnter(Collider collider)
         {
-            StartCoroutine(Press());
+            if (!CD)
+            {
+                StartCoroutine(Press());
+            }
         }
 
         private IEnumerator Press()
         {
+            CD = true;
             Plugin.instance.ChangeToNight();
             yield return new WaitForSeconds(0.25f);
+            CD = false;
+        }
+    }
+
+    public class SunsetButton : GorillaPressableButton
+    {
+        public static bool CD = false;
+        private void OnTriggerEnter(Collider collider)
+        {
+            if (!CD)
+            {
+                StartCoroutine(Press());
+            }
+        }
+
+        private IEnumerator Press()
+        {
+            CD = true;
+            Plugin.instance.ChangeToSunset();
+            yield return new WaitForSeconds(0.25f);
+            CD = false;
         }
     }
 }
